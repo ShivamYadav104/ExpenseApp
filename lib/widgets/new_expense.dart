f9a1148b269df075d67150513bc8d3980668342b
Shivam104V <sy8684507@gmail.com>
2025-07-09 02:08:58 +0530

    Update new_expense.dart

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -195,7 +195,7 @@ class _NewExpenseState extends State<NewExpense> {
                             controller: _amountController,
                             keyboardType: TextInputType.number,
                             decoration: InputDecoration(
-                              prefixText: '₹ ',
+                              prefixText: '\$ ',
                               label: Text('Amount'),
                             ),
                           ),
